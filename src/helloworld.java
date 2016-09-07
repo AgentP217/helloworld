a9826c86d70a59acbc6b75f4c90589656f890ba3
AgentP217 <agentp217@gmail.com>
2016-09-07 13:13:22 +0700

    Revert "Revert "Created a HelloWorld project""
    This reverts commit 8ffc99c45b42c20e2947fc1f711819d629952916.

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Acer on 04/09/16.
+ */
+public class helloworld
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Hello, World ^-^");
+    }
+}
